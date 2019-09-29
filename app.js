36d38160fea05528bf06e400461fdbd41b0b9c8b
divyaathiyarath <divyaathiyarath@gmail.com>
2019-09-29 14:55:22 +0530

    final

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var mongoose=require('mongoose');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://edppapi.herokuapp.com' );
+    res.setHeader('Access-Control-Allow-Origin', 'https://edpp.herokuapp.com' );
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
